85f0d45747e4000f31d9a54aeaf397f631e9c10b
AnnPla <anna.platek2222@gmail.com>
2020-01-25 23:41:46 +0100

    add spawning points for Attackers in each rows (spawn script + spawn prefab)

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] bool spawn = true;
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    [SerializeField] Object Enemy;
+
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+
+    private void SpawnAttacker()
+    {
+        Instantiate(Enemy, transform.position, transform.rotation);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
