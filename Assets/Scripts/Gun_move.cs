16028b44fc0e4192af961d2cbda2c333617e38de
AnnPla <anna.platek2222@gmail.com>
2020-02-01 14:25:18 +0100

    add moving projectile to cactus

---
+++ Assets/Scripts/Gun_move.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun_move : MonoBehaviour
+{
+    float gunSpeed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * gunSpeed * Time.deltaTime);
+
+    }
+}
