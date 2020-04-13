5980350949f8157a59a75cbfaf89e79158078823
AnnPla <anna.platek2222@gmail.com>
2020-04-13 18:14:11 +0200

    add Pause, Play and Acceleration buttons

---
+++ Assets/Scripts/PausePlaySpeed.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausePlaySpeed : MonoBehaviour
+{
+
+    public void PauseButton()
+    {
+    
+        Time.timeScale = 0;
+    }
+
+    public void PlayButton()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void SpeedButton()
+    {
+        Time.timeScale = 2;
+    }
+
+    //private void OnMouseDown()
+    //{
+    //    var buttons = FindObjectsOfType<PausePlaySpeed>();
+    //    foreach (PausePlaySpeed button in buttons)
+    //    {
+    //        button.GetComponent<SpriteRenderer>().color = new Color32(70, 70, 70, 255);
+    //    }
+    //
+    //    GetComponent<SpriteRenderer>().color = Color.white;
+    //    //FindObjectOfType<DefenderSpawner>().SetSelectedDefender(defenderPrefab);
+    //}
+
+}
